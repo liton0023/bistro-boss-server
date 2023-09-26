f421a0670c52b2b808e7c406800504d7f94e3ccb
Md Liton Mahmud <litonmahmud0023@gmail.com>
2023-09-26 23:43:31 +0600

    payment-system complate

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const app = express();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleweres
@@ -58,6 +59,7 @@ async function run() {
     const reviewsCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
     const usersCollection = client.db("bistroDb").collection("users");
+      const paymentCollection = client.db("bistroDb").collection("payments");
 
 
      // Warning: use verifyJWT before using verifyAdmin
@@ -123,7 +125,7 @@ async function run() {
       const query = { email: email };
       // console.log(query)
       const user = await usersCollection.findOne(query);
-      console.log('user:',user);
+      // console.log('user:',user);
       const result = { admin: user?.role === "admin" };
       console.log("admin:", result);
       res.send(result);
@@ -142,36 +144,59 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/menu/:id',verifyJWt ,verifyAdmin, async(rew ,res)=>{
-
-      const id =rew.params.id
-      const query ={ _id: new ObjectId(id)};
+    app.delete('/menu/:id', verifyJWt, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
       const result = await menuCollection.deleteOne(query);
       res.send(result);
+    })
+
+
+    //  update menu
+    // app.get('/menu/:id', verifyJWt ,verifyAdmin ,async(req ,res)=>{
+    //   const id =req.params.id;
+    //   console.log(id)
+    //   const result = await menuCollection.findOne(id);
+    //   res.send(result)
+
+    // })
+
+    app.put('/menu/:id',verifyJWt, verifyAdmin,async(req,res)=>{
+      const id =req.params.id;
+      console.log(id)
+      const filter ={ _id: id};
+      const {name, price, recipe ,category,image} =req.body;
+      console.log(name)
+      const updateItem ={
+            name : name,
+            price :price,
+            recipe: recipe,
+            category : category ,
+            image : image
+      }
+      console.log(updateItem)
+      const option = {new : true}
+
+      try {
+        const result = await menuCollection.replaceOne(filter, updateItem,option);
+    
+        if (result.modifiedCount === 1) {
+          console.log('Document replaced successfully');
+        } else {
+          console.log('Document not found or not replaced');
+        }
+        console.log(result)
+      } catch (err) {
+        console.error('Error replacing document:', err);
+      }
+      
+      // const result = await menuCollection.updateOne(filter, updateItem,option);
+      // res.json(result)   
+          // console.log(result)
 
     })
-    //  update menu
-    app.patch('/menu/:id',verifyJWt, verifyAdmin,async(req,res)=>{
-      const id =req.params.id;
-      const { name, price, category, recipe } = req.body;
-      const filter ={_id: new ObjectId(id)};
-      const updateDoc={
-        $set:{
-          name,
-          category,
-          price,
-          recipe
-        }
-      }
-      const result= await menuCollection.updateOne(filter , updateDoc);
-      if (result.modifiedCount === 1) {
-        res.status(200).json({ message: "Item updated successfully" });
-      } else {
-        res.status(404).json({ message: "Item not found or not updated" });
-      }
-      console.log(result)
-      res.send(result);
-    })
+
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
@@ -212,6 +237,113 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
+
+
+        // create payment intent
+    app.post('/create-payment-intent', verifyJWt, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+
+
+        // payment related api
+    app.post('/payments', verifyJWt, async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+
+      const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+      const deleteResult = await cartCollection.deleteMany(query)
+
+      res.send({ insertResult, deleteResult });
+    })
+
+    app.get('/admin-stats', verifyJWt, verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // best way to get sum of the price field is to use group and sum operator
+      /*
+        await paymentCollection.aggregate([
+          {
+            $group: {
+              _id: null,
+              total: { $sum: '$price' }
+            }
+          }
+        ]).toArray()
+      */
+
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce( ( sum, payment) => sum + payment.price, 0)
+
+      res.send({
+        revenue,
+        users,
+        products,
+        orders
+      })
+    })
+
+
+    /**
+     * ---------------
+     * BANGLA SYSTEM(second best solution)
+     * ---------------
+     * 1. load all payments
+     * 2. for each payment, get the menuItems array
+     * 3. for each item in the menuItems array get the menuItem from the menu collection
+     * 4. put them in an array: allOrderedItems
+     * 5. separate allOrderedItems by category using filter
+     * 6. now get the quantity by using length: pizzas.length
+     * 7. for each category use reduce to get the total amount spent on this category
+     * 
+    */
+    app.get('/order-stats', verifyJWt, verifyAdmin, async(req, res) =>{
+      const pipeline = [
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'menuItems',
+            foreignField: '_id',
+            as: 'menuItemsData'
+          }
+        },
+        {
+          $unwind: '$menuItemsData'
+        },
+        {
+          $group: {
+            _id: '$menuItemsData.category',
+            count: { $sum: 1 },
+            total: { $sum: '$menuItemsData.price' }
+          }
+        },
+        {
+          $project: {
+            category: '$_id',
+            count: 1,
+            total: { $round: ['$total', 2] },
+            _id: 0
+          }
+        }
+      ];
+
+      const result = await paymentCollection.aggregate(pipeline).toArray()
+      res.send(result)
+
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
